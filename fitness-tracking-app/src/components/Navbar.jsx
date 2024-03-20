44cc6ed84707c9e092b858308f70b56e6a492cd9
Dinah <dinah.ngatia@student.moringaschool.com>
2024-03-20 03:54:07 +0300

    React App

--- fitness-tracking-app/src/components/Navbar.jsx
+++ fitness-tracking-app/src/components/Navbar.jsx
@@ -1,3 +1,15 @@
 import React from "react";
 import { Stack } from "@mui/material";
 import { Link } from "react-router-dom";
+
+const Navbar = () => {
+
+
+    return (
+        <Stack>
+            <Link to={} />
+        </Stack>
+    );
+}
+
+export default Navbar;
